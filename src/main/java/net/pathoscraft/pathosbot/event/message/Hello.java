15e3431fb0bb959c5ea1108f34212c244bcbd0d2
QuackieMackie <dev.quackiemackie@gmail.com>
2025-03-04 10:13:21 +0000

    Refactor build config and modularize message handling
    Replaced `build.gradle.kts` with `build.gradle` to introduce new plugins and functionalities, such as `SSH` and `JUnit` configurations. Modularized the `!hello` command by delegating its functionality to a new `Hello` class. Updated project Java version to 21, refined README, and added IntelliJ configurations.

---
+++ src/main/java/net/pathoscraft/pathosbot/event/message/Hello.java
@@ -0,0 +1,20 @@
+package net.pathoscraft.pathosbot.event.message;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class Hello {
+    public static void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
+
+        Message message = event.getMessage();
+        String content = message.getContentRaw();
+
+        if (content.equals("!hello")) {
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("Hello").queue();
+        }
+    }
+
+}
